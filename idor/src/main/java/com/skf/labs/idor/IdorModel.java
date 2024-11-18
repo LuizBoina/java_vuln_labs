e0720ab3ee7c941fa3ec143867b5cf2a182334ee
LuizBoina <luizfelipeboina@outlook.com>
2024-11-18 20:06:44 +0000

    fixing IDOR lab

--- idor/src/main/java/com/skf/labs/idor/IdorModel.java
+++ idor/src/main/java/com/skf/labs/idor/IdorModel.java
@@ -16,28 +16,38 @@ import com.itextpdf.text.DocumentException;
 
 public class IdorModel {
 
-public static List<Integer> pdfs = new ArrayList<Integer>();
-private static int min = 1;
-private static int max = 100;
-private static Random rand;
+    public static List<Integer> pdfs = new ArrayList<Integer>();
+    private static int min = 1;
+    private static int max = 100;
+    private static Random rand;
 
-   public static int createPdf(String message){
-    try{
-       int pdfId = rand.nextInt(max-min+1)+min;
-       Document document = new Document();
-       PdfWriter.getInstance(document, new FileOutputStream(pdfId+".pdf"));
-       document.open();
-       Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-       Chunk chunk = new Chunk(message, font);
-       document.add(chunk);
-       document.close();
-       pdfs.add(pdfId);
-       return pdfId;
-    }catch(FileNotFoundException e){
-        e.printStackTrace();
-    }catch(DocumentException e){
-        e.printStackTrace();
+    public static void init(){
+       createPdfPool();
     }
-    return -1;
-   }  
+
+    public static void createPdfPool(){
+        rand = new Random();
+        for(int i = 0; i < 60; i++) createPdf("Try again!");
+        createPdf("You have found the secret pdf, congratulations!");
+    }
+
+    public static int createPdf(String message){
+        try{
+        int pdfId = rand.nextInt(max-min+1)+min;
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(pdfId+".pdf"));
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+        Chunk chunk = new Chunk(message, font);
+        document.add(chunk);
+        document.close();
+        pdfs.add(pdfId);
+        return pdfId;
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(DocumentException e){
+            e.printStackTrace();
+        }
+        return -1;
+    }  
 }
